60a3f88b309f08fdecf4b563d41fc7363d5a2a41
pramataroff <milenpramatarov@gmail.com>
2024-05-14 23:14:06 +0300

    .

---
+++ paskal piramida/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace paskal_piramida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+
+            int[][] triangle = new int[h + 1][];
+
+            for (int row = 0; row < h; row++)
+            {
+
+                triangle[row] = new int[row + 1];
+
+            }
+
+            triangle[0][0] = 1;
+
+            for (int row = 0; row < h - 1; row++)
+            {
+
+                for (int col = 0; col <= row; col++)
+                {
+
+                    triangle[row + 1][col] += triangle[row][col];
+
+                    triangle[row + 1][col + 1] += triangle[row][col];
+
+                }
+
+            }
+
+            
+            for (int row = 0; row < h; row++)
+            {
+                for (int col = 0; col <= row; col++)
+                {
+                    Console.Write(triangle[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}        
